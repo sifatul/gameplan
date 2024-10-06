8f530aa2f9fe971c5eeb1bf07db1281bced8ffbb
mdsifatul <mdsifatulislam@airasia.com>
2024-10-07 07:46:42 +0800

    chore: icon update

--- src/MatchContainer/MatchList.jsx
+++ src/MatchContainer/MatchList.jsx
@@ -4,15 +4,18 @@ import { IoIosDoneAll } from 'react-icons/io';
 import { useMatch } from '../context/MatchContext';
 import { usePlayers } from '../context/PlayersContext';
 import { generateRounds } from '../utils/gameUtil';
+import { MdCheckCircleOutline } from "react-icons/md";
+import { MdCheckCircle } from "react-icons/md";
+
 
 function MatchListPage(props) {
   
   const {matches, setMatchList, changeMatchStatus} = useMatch();
   const { players, addPlayer, removePlayer, changePlayerName, getActivePlayers } = usePlayers();
  
-  const handleGameOver = useCallback(
-    (roundIdx) => changeMatchStatus(roundIdx, false),
-    [matches, setMatchList],
+  const handleGameStatusUpdate = useCallback(
+    (roundIdx, status) => changeMatchStatus(roundIdx, status),
+    [],
   );
 
 
@@ -39,7 +42,8 @@ function MatchListPage(props) {
                   <IoMdPeople className={isActive ? '' : 'icon-inactive'} size={30} style={{ marginRight: '10px' }} />
                   <span> {team[0]}</span> & <span>{team[1]}</span>
                 </div>
-                <IoIosDoneAll size={30} onClick={e => handleGameOver(roundIdx)} />
+                {isActive && <MdCheckCircleOutline size={20} onClick={e => handleGameStatusUpdate(roundIdx, false)} />}
+                {!isActive && <MdCheckCircle size={20}  onClick={e => handleGameStatusUpdate(roundIdx, true)} />}
               </div>
             </div>
           );
