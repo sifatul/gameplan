0769e6197f86adbdd5480418fe22bfd96104e2c0
mdsifatul <mdsifatulislam@airasia.com>
2024-10-07 07:07:24 +0800

    chore: icons added

--- src/MatchContainer/ActivePlayerList.jsx
+++ src/MatchContainer/ActivePlayerList.jsx
@@ -3,13 +3,17 @@ import { IoIosWarning, IoMdPeople } from 'react-icons/io';
 import { usePlayers } from '../context/PlayersContext';
 import { useMatch } from '../context/MatchContext';
 import { generateRounds } from '../utils/gameUtil';
+import { FaBed } from "react-icons/fa";
+import { AiOutlineCheckCircle } from "react-icons/ai";
+
+
 
 function ActivePlayerListPage() {
   const { players, setPlayerStatus, getActivePlayers } = usePlayers();
   const { matches, setMatchList, changeMatchStatus, getActiveMatches } = useMatch();
 
   // Use callback to update player's active state
-  const setInActivePlayer = useCallback(idx => setPlayerStatus(idx, false), []);
+  const handlePlayerStatusChange = useCallback((idx,status) => setPlayerStatus(idx, status), []);
 
 
 
@@ -25,7 +29,10 @@ function ActivePlayerListPage() {
                 <IoMdPeople size={30} className={isActive ? '' : 'icon-inactive'} style={{ marginRight: '10px' }} />
                 <span>{player.name}</span>
               </div>
-              <IoIosWarning size={20} onClick={() => setInActivePlayer(index)} />
+              {/* <FaBed /> */}
+
+              {!isActive && <FaBed size={20} onClick={() => handlePlayerStatusChange(index, true)} />}
+              {isActive && <AiOutlineCheckCircle size={20} onClick={() => handlePlayerStatusChange(index, false)} />}
             </div>
           );
         })}
