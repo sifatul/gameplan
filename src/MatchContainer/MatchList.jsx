92334cb57aaecb3588791968d83a758b6a18a959
mdsifatul <mdsifatulislam@airasia.com>
2024-10-11 16:22:33 +0800

    chore: css update

--- src/MatchContainer/MatchList.jsx
+++ src/MatchContainer/MatchList.jsx
@@ -4,6 +4,8 @@ import { MdCheckCircle, MdCheckCircleOutline } from 'react-icons/md';
 import { useMatch } from '../context/MatchContext';
 import { usePlayers } from '../context/PlayersContext';
 import { scheduleMatches } from '../utils/gameUtil';
+import { FaRegCircle } from "react-icons/fa";
+
 
 function MatchListPage(props) {
   const { matches, setMatchList, changeMatchStatus } = useMatch();
@@ -29,28 +31,30 @@ function MatchListPage(props) {
           const { isActive, team1, team2 } = round;
           return (
             <li key={`round-${roundIdx}`} className={`match-team ${isActive ? 'active' : ''}`}>
-              <div className="flex-row">
+              <div className="flex-row w-35 align-left">
                 {/* <IoMdPeople className={isActive ? '' : 'icon-inactive'} size={20} style={{ marginRight: '10px' }} /> */}
                 <div>
                   <div className="team-name">{team1[0]}</div>
                   <div className="team-name">{team1[1]}</div>
                 </div>
               </div>
-              <div className="flex-row">
+              <div className="align-center w-15">
                 <span className='bold-text'>VS</span>
               </div>
-              <div className="flex-row">
+              <div className="flex-row grow-2 w-35">
                 {/* <IoMdPeople className={isActive ? '' : 'icon-inactive'} size={20} style={{ marginRight: '10px' }} /> */}
                 <div>
                   <div className="team-name">{team2[0]}</div>
                   <div className="team-name">{team2[1]}</div>
                 </div>
               </div>
+              <div className='flex-row w-15 align-center'>
               {isActive ? (
-                <MdCheckCircle size={25} onClick={() => handleGameStatusUpdate(roundIdx, false)} className="status-icon" />
+                <FaRegCircle size={30} onClick={() => handleGameStatusUpdate(roundIdx, false)} className="status-icon" />
               ) : (
-                <MdCheckCircleOutline size={25} onClick={() => handleGameStatusUpdate(roundIdx, true)} className="status-icon" />
+                <MdCheckCircleOutline size={30} onClick={() => handleGameStatusUpdate(roundIdx, true)} className="status-icon" />
               )}
+              </div>
             </li>
           );
         })}
